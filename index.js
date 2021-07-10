bb7933c29be9f520dc935c81c35334d9a68d5ed2
Gaby <astrogrilledveggie@gmail.com>
2021-07-10 20:01:45 +0800

    multer-veryfi-mongodb upload-parse-addToCollection routing works

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const userRouter = require('./routers/user_router')
 //      HOST
 // =======================
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 7000
 const mongoURI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_NAME}`
 
 mongoose.set('useFindAndModify', false)
@@ -22,11 +22,11 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.use('/api/v1/item', itemRouter)
-app.use('/api/v1/users', userRouter)
+app.use('/api/v1/user', userRouter)
 
 mongoose.connect( mongoURI, { useNewUrlParser: true, useUnifiedTopology: true } )
   .then(response => {
     app.listen(port, () => {
-      console.log(`Animal Shelter app listening on port: ${port}`)
+      console.log(`LeftWat app listening on port: ${port}`)
     })
   })
--- routers/purchasedItems_router.js
+++ routers/purchasedItems_router.js
@@ -1,8 +1,26 @@
 const express = require('express')
 const router = express.Router()
-const purchasedItemController = require('../controllers/purchasedItems_controller')
+const multer = require('multer')
+const itemController = require('../controllers/purchasedItems_controller')
+
+let storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public/')
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + '.pdf')
+  }
+})
+
+let upload = multer({ storage: storage })
 
 //index route
-router.get('/', purchasedItemController.index)
+router.get('/', itemController.index)
+
+// upload receipt route
+router.post('/upload', upload.single('testReceipt'), itemController.uploadReceipt)
+
+// confirm receipt route
+router.post('/upload/confirm', itemController.confirmReceipt)
 
 module.exports = router
