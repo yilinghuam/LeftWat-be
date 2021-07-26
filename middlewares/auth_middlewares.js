a422a17d595d3ad70ba63e71bbe237471ddb451f
Yi ling <lingy93@gmail.com>
2021-07-27 00:07:06 +0800

    check token

--- middlewares/auth_middlewares.js
+++ middlewares/auth_middlewares.js
@@ -5,7 +5,7 @@ module.exports = {
     authenticatedOnly: (req, res, next) => {
         console.log(req)
         //check if token exists
-        const token = req.cookies.access_token
+        const token = req.headers.auth_token
         if(!token) {
             return res.status(403) //forbidden
 
