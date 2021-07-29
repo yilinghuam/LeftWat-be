51bb2384c988100af13b6d726e5bce8a3f999528
Yi ling <lingy93@gmail.com>
2021-07-29 14:45:01 +0800

    try aggregate

--- controllers/pricecomparison_controller.js
+++ controllers/pricecomparison_controller.js
@@ -27,9 +27,22 @@ module.exports = {
     show: async(req,res) => {
         let item = _.kebabCase(req.headers.query)
         try {
-            const itemData = await itemModel.find({'userID.email':req.email,slug:item})
+            const itemData = await itemModel.aggregate([
+                {$match:{'userID.email':req.email,slug:item}},
+                {$group:{
+                    slug:item,
+                    itemPrice: {$push: "$itemPrice"},
+                    maxPrice: {$max: "$itemPrice"},
+                    minPrice: {$min:"$itemPrice"},
+                    averagePrice: {$avg:"$itemPrice"}
+                    itemLabel: {$push: {$dateToString: {format:"%Y-%m-%d", date:"$createdAt"}}}
+                }}
+            ])
+            // const allItemData = await itemModel.find({slug:item})
             // need to include 5 most recent receipt data  that 
             console.log(itemData)
+            // itemName,itemPrice,createdAt
+            
             return res.json(itemData)
         } catch (error) {
             res.statusCode = 400
